281de7025caa299a7273799312fd9c3ce4a0888d
Kristoffer Framstad <kristoffer.framstad@finn.no>
2018-10-01 08:53:52 +0200

    First working version

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,16 @@
 #include <Keypad.h>
 #include <Secrets.h>
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
+const byte ROWS = 4;
+const byte COLS = 4;
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {10, 9, 8, 7};
+byte colPins[COLS] = {6, 5, 4, 3};
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -32,21 +31,23 @@ void triggerGarageDoor () {
 
 void loop() {
     char key = keypad.getKey();
-    if (key && key == PASS_CODE[index]) {
-        Serial.print("match! key=");
-        Serial.print(key);
-        Serial.print(", index = ");
-        Serial.println(index);
-        index++;
-        if (index == 4) {
-          triggerGarageDoor();
+    if (key) {
+      if (key && key == PASS_CODE[index]) {
+          Serial.print("match! key=");
+          Serial.print(key);
+          Serial.print(", index = ");
+          Serial.println(index);
+          index++;
+          if (index == PASS_CODE_LENGTH) {
+            triggerGarageDoor();
+            index = 0;
+          }
+      } else {
+          Serial.print("NO match! key=");
+          Serial.print(key);
+          Serial.print(", index = ");
+          Serial.println(index);
           index = 0;
-        }
-    } else {
-        Serial.print("NO match! key=");
-        Serial.print(key);
-        Serial.print(", index = ");
-        Serial.print(index);
-        index = 0;
-    }
+      }
+  }
 }
